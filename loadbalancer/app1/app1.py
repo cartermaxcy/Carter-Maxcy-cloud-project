a3c048277e242681c51f4e7db3612d2ebbb00981
Carter Maxcy <cartermaxcy@gmail.com>
2026-01-20 23:02:09 +0000

    Add load balancing demonstration.

---
+++ loadbalancer/app1/app1.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"Response from Server 1 (Hostname: {socket.gethostname()})"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
