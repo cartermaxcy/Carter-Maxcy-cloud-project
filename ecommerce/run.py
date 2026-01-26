8ae9e1dff09a6746617a6f01c5c075013ec4b9f5
Carter Maxcy <cartermaxcy@gmail.com>
2026-01-26 00:04:39 +0000

    Add basic site map.

--- ecommerce/run.py
+++ ecommerce/run.py
@@ -4,7 +4,14 @@ app = create_app()
 
 @app.route('/')
 def welcome():
-    return 'Welcome User!'
+    return """
+    Welcome User!
+    Append the following to the URL to view each page:
+    /browser/register,
+    /browser/login,
+    /browser/inventory,
+    /browser/products
+    """
 
 if __name__ == '__main__':
     app.run()
